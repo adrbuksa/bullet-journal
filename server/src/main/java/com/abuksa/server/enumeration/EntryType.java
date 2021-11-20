195df1d53c974715bd2b42460eb3ceb971059550
adrbuksa <adr.buksa@gmail.com>
2021-11-20 23:22:38 +0100

    typo fix

--- server/src/main/java/com/abuksa/server/enumeration/EntryType.java
+++ server/src/main/java/com/abuksa/server/enumeration/EntryType.java
@@ -1,11 +1,11 @@
 package com.abuksa.server.enumeration;
 
 public enum EntryType {
-    NOTE("NOTE"), TASK("TASK"), EVEMT("EVENT");
-
+    NOTE("NOTE"), TASK("TASK"), EVENT("EVENT");
     private final String type;
 
     EntryType(String type) { this.type = type; }
 
     public String getType() { return this.type; }
+
 }
