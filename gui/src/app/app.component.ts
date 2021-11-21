a1ea65dd78f35e5967bb1f3c0d1999e7a23a980a
adrbuksa <adr.buksa@gmail.com>
2021-11-21 15:40:23 +0100

    initialized app component with starting data

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { EntryService } from './service/entry.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gui';
+  appState$: Observable<AppState<CustomResponse>>;
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+
+  constructor(private entryService: EntryService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.entryService.entries$.pipe(
+      map(response => {
+        this.dataSubject.next(response);
+        return { dataState: DataState.LOADED_STATE, appData: { ...response, data: { entries: response.data.entries } } };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
 }
